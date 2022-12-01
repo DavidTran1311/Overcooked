76d774c0b55b40578a65383d60da630eb062d635
DavidTran1311 <114709085+DavidTran1311@users.noreply.github.com>
2022-12-01 01:22:09 -0500

    Added_Files_to_github

---
+++ prototype/Assets/Object_Interaction.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Interaction : MonoBehaviour
+{
+    private Transform PickUpPoint;
+    private Transform player;
+
+    public float PickUpDistance;
+    public float ForceMulti;
+
+    public bool ReadyToThrow;
+    public bool ItemisPicked;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+        player = GameObject.Find("Player").transform;
+        PickUpPoint = GameObject.Find("Interact").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E)&& ItemisPicked==true && ReadyToThrow)
+        {
+            ForceMulti += 300 * Time.deltaTime;
+                
+        }
+
+        PickUpDistance = Vector3.Distance(player.position, transform.position);
+
+        if (PickUpDistance <= 10)
+        {
+            if(Input.GetKeyDown(KeyCode.R)  && ItemisPicked==false && PickUpPoint.childCount < 1)
+            {
+
+                GetComponent<Rigidbody>().useGravity = false;
+                GetComponent<BoxCollider>().enabled = false;
+                this.transform.position = PickUpPoint.position;
+                this.transform.parent = GameObject.Find("Interact").transform; 
+
+                ItemisPicked = true;
+                ForceMulti = 0;
+
+            }
+        
+        
+        }
+        if (Input.GetKeyUp(KeyCode.E) && ItemisPicked == true)
+        {
+
+            ReadyToThrow = true;
+            if (ForceMulti > 10)
+            {
+                rb.AddForce(player.transform.forward * ForceMulti);
+                this.transform.parent = null;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<BoxCollider>().enabled = true;
+                ItemisPicked = false;   
+
+                ForceMulti = 0;
+                ReadyToThrow = false;
+            }
+            ForceMulti = 0;
+        }
+    }
+
+}
